913423c5b1ec436fefc2f73aabc756c5b86f3431
GianmarcoBramucci <giangian797@gmail.com>
2024-05-27 23:59:24 +0200

    basic ese

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,13 @@
 @section('content')
 <main>
     <h1>Home page</h1>
+    <ul>
+        @foreach ($movies as $movie)
+        <li>
+            <h2> {{ $movie->title }}</h2>        
+        </li>
+        @endforeach    
+    </ul>
 </main>
 
 @endsection
